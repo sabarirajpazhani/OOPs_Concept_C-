802356197f5b23d8d416906c1ce806f8eb84fb9b
sabarirajpazhani <sabarirajpazhani@gmail.com>
2025-05-01 10:30:28 +0530

    Add project files.

---
+++ OOPs_Concept_C#/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace OOPs_Concept_C
+{
+    class Bike
+    {
+        public string BikeNmae = "Yamaha";
+
+        public void Display()
+        {
+            Console.WriteLine("Bike Name: "+ BikeNmae);
+        }
+    }
+    public class OOPs_Concepts_C
+    {
+        static void Main(string[] args)
+        {
+            Bike obj = new Bike();
+
+            Console.WriteLine(obj.BikeNmae);
+            obj.Display();
+        }
+    }
+}
+
+
+//Output
+
+//Yamaha
+//Bike Name: Yamaha
